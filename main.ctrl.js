27ff79b42917e45d503f30dc0d1c0c5a1a900af5
leon <leon@revillwebdesign.com>
2015-01-29 20:33:15 +0000

    Ensure years are int not string

--- main.ctrl.js
+++ main.ctrl.js
@@ -5,27 +5,27 @@ angular.module('app').controller("MainController", function(){
     vm.shows = [
         {
             title: 'Game of Thrones',
-            year: '2011',
+            year: 2011,
             favorite: true
         },
         {
             title: 'Walking Dead',
-            year: '2010',
+            year: 2010,
             favorite: false
         },
         {
             title: 'Firefly',
-            year: '2002',
+            year: 2002,
             favorite: true
         },
         {
             title: 'Banshee',
-            year: '2013',
+            year: 2013,
             favorite: true
         },
         {
             title: 'Greys Anatomy',
-            year: '2005',
+            year: 2005,
             favorite: false
         }
     ];
